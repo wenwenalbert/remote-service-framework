9a7455dadd6107dae7569f7baaf19645fd04b950
albert <zhaowenbo@qiyi.com>
2016-04-25 18:23:25 +0800

    update

--- rsf-app/src/main/java/com/shanghai/albert/App.java
+++ rsf-app/src/main/java/com/shanghai/albert/App.java
@@ -1,5 +1,9 @@
 package com.shanghai.albert;
 
+import com.shanghai.albert.timer.TimerJob;
+
+import java.util.Timer;
+
 /**
  * App
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        Timer timer = new Timer();
+        long startDelay = 1000; //Unit(ms)
+        long repeatInterval = 10000;  //Unit(ms)
+
+        timer.schedule(new TimerJob("job1"), startDelay, repeatInterval);
+
+        timer.schedule(new TimerJob("job2"), startDelay * 2, repeatInterval * 2);
+
 
     }
 }
